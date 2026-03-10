c956e31bfdd815a9cf492e21cfc78a1ab46bb6cd
LiHua5487 <3572257484@qq.com>
2026-03-10 16:10:07 +0800

    change config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://lihua5487.github.io/Notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
